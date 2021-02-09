85a612306504d603d29be2989b8a5a4ebad49252
mortemors <jefersongb@outlook.com.br>
2021-02-09 19:04:48 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -1,6 +1,6 @@
 export class MissingParamError extends Error {
   constructor (paramName: string) {
-    super(`Invalid param: ${paramName}`)
-    this.name = 'InvalidParamError'
+    super(`Missing param: ${paramName}`)
+    this.name = 'MissingParamError'
   }
 }
