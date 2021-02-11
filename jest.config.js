f6031f7069e3b84c1c1b031515cdc43ffa308c36
mortemors <jefersongb@outlook.com.br>
2021-02-10 23:10:01 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,13 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
   coveragePathIgnorePatterns: [
     '<rootDir>/src/presentation/protocols/index.ts',
-    '<rootDir>/src/presentation/controllers/signup/signup-protocols.ts'
+    '<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts'
   ]
 }
