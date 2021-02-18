6783c31fde2a72a9a4df9538dca40593ed651634
Morte Mors <jefersongb@outlook.com.br>
2021-02-17 23:40:10 -0300

    fix: import path

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,9 @@ module.exports = {
   },
   coveragePathIgnorePatterns: [
     '<rootDir>/src/presentation/protocols/index.ts',
-    '<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
-    '<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts'
+    '<rootDir>/src/presentation/controllers/signup/signup-controller-protocols.ts',
+    '<rootDir>/src/presentation/controllers/login/login-controller-protocols.ts',
+    '<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts',
+    '<rootDir>/src/data/usecases/authentication/db-authentication-protocols.ts'
   ]
 }
