f7e5482b1c952246f021264680c7edd0812c5733
Morte Mors <jefersongb@outlook.com.br>
2021-02-19 01:23:56 -0300

    chore: enable debug with code

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,7 +4,6 @@ import { readdirSync } from 'fs'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  // eslint-disable-next-line node/no-path-concat
   readdirSync(`${__dirname}/../routes`).map(async file => {
     if (!file.includes('.test.')) {
       (await import(`../routes/${file}`)).default(router)
